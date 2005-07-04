41dff53dd8a491a350e6024c9ef0656c3acb2d4a
Colin Watson <colin.watson@canonical.com>
2005-07-04 12:46:59 +0000

    implement most of oem-config and oem-config-menu in Wizard

--- pygtk/wizard.py
+++ pygtk/wizard.py
@@ -6,20 +6,87 @@
 
 import sys
 import os
+import re
+import optparse
 import pygtk
 pygtk.require('2.0')
 import gtk
 import gtk.glade
 import debconf
 
+menudir = '/usr/share/oem-config/menu'
 moduledir = '/usr/lib/oem-config/pygtk'
 stepsdir = os.path.join(moduledir, 'steps')
+
+menu_line_re = re.compile(r'(.*?): (.*)')
 
 from steps.timezone import *
 
 class Wizard:
-    def __init__(self, db):
-        self.db = db
+    def __init__(self, includes=None, excludes=None):
+        self.menus = {}
+        for menu in [f for f in os.listdir(menudir) if f.endswith('.mnu')]:
+            name = '.'.join(menu.split('.')[:-1])
+
+            # Always include the exit item. Otherwise, check includes and
+            # excludes.
+            if name != 'exit':
+                if includes is not None and not name in includes:
+                    continue
+                if excludes is not None and name in excludes:
+                    continue
+
+            # If there is a test script, check that it succeeds.
+            testscript = os.path.join(menudir, '%s.tst' % name)
+            if os.access(testscript, os.X_OK):
+                if os.spawnl(os.P_WAIT, testscript) != 0:
+                    continue
+
+            self.menus[name] = {}
+            menufile = open(os.path.join(menudir, menu))
+            for line in menufile:
+                match = menu_line_re.match(line)
+                if match is not None:
+                    self.menus[name][match.group(1).lower()] = match.group(2)
+
+            # If there is an Asks: field, match it against the list of
+            # question names in the debconf database.
+            if 'asks' in self.menus[name]:
+                asks_re = self.menus[name]['asks']
+                asks = []
+
+                # It isn't possible to use debconf-copydb after the debconf
+                # frontend has started up, so we have to resort to this
+                # nasty hack: we process the Asks: fields, stuff them into
+                # the environment, start the debconf frontend (re-execing
+                # ourselves) and fetch the processed Asks: fields back out
+                # of the environment again.
+                #
+                # The best fix for this mess is to make debconf-copydb treat
+                # its source database as read-only. Unfortunately, layering
+                # issues inside debconf make this difficult for the time
+                # being.
+
+                magic_env = 'OEM_CONFIG_ASKS_%s' % name
+                if magic_env in os.environ:
+                    self.menus[name]['asks'] = os.environ[magic_env].split(' ')
+                else:
+                    # TODO: os.popen() doesn't take a list, so we have to
+                    # quote metacharacters by hand. Once we're entirely
+                    # comfortable with relying on Python 2.4, we can use
+                    # subprocess.call() instead.
+                    asks_re = re.sub(r'\W', r'\\\g<0>', asks_re)
+                    for line in os.popen(
+                            'debconf-copydb configdb pipe' +
+                            ' --config=Name:pipe --config=Driver:Pipe' +
+                            ' --config=InFd:none --pattern=%s' % asks_re):
+                        line = line.rstrip('\n')
+                        if line.startswith('Name: '):
+                            asks.append(line[6:])
+                    self.menus[name]['asks'] = asks
+                    os.environ[magic_env] = ' '.join(asks)
+
+        self.start_debconf()
 
         self.glades = {}
         for glade in [f for f in os.listdir(stepsdir) if f.endswith('.glade')]:
@@ -33,7 +100,25 @@ class Wizard:
             if hasattr(mod, 'stepname'):
                 self.steps[name] = getattr(mod, mod.stepname)
 
-    def run(self, step):
+    def start_debconf(self):
+        debconf.runFrontEnd()
+        self.db = debconf.Debconf()
+
+        for name in self.menus:
+            self.menus[name]['description'] = \
+                self.db.metaget('oem-config/menu/%s' % name, 'description')
+
+    # Get a list of the menu items, sorted by their Order: fields.
+    def get_menu_items(self):
+        def menu_sort(x, y):
+            return cmp(int(self.menus[x]['order']),
+                       int(self.menus[y]['order']))
+
+        items = self.menus.keys()
+        items.sort(menu_sort)
+        return items
+
+    def run_step(self, step):
         xml = gtk.glade.XML(self.glades[step])
         xml.signal_autoconnect(self)
         dialog = xml.get_widget('dialog')
@@ -41,7 +126,41 @@ class Wizard:
         stepper = self.steps[step](self.db, xml)
         stepper.run()
 
+    def run(self):
+        items = self.get_menu_items()
+        index = 0
+        while index < len(items):
+            item = items[index]
+            # Set as unseen all questions that we're going to ask.
+            if 'asks' in self.menus[item]:
+                for name in self.menus[item]['asks']:
+                    print >>sys.stderr, 'asks', item, name
+                    self.db.fset(name, 'seen', 'false')
+
+            # Is there a custom frontend for this item? If so, run it.
+            # TODO: backup handling
+            if item in self.steps:
+                self.run_step(item)
+
+            # Run the pure-debconf menu item.
+            # TODO: do something useful on failure
+            # TODO: backup handling
+            os.spawnl(os.P_WAIT, os.path.join(menudir, item))
+
+            # Did this menu item finish the configuration process?
+            if ('exit-menu' in self.menus[item] and
+                self.menus[item]['exit-menu'] == 'true'):
+                break
+
+            index += 1
+
 if __name__ == '__main__':
-    debconf.runFrontEnd()
-    db = debconf.Debconf()
-    Wizard(db).run(sys.argv[1])
+    parser = optparse.OptionParser()
+    parser.add_option('-i', '--include', action='append', metavar='ITEM',
+                      help="Display this menu item.")
+    parser.add_option('-e', '--exclude', action='append', metavar='ITEM',
+                      help="Don't display this menu item.")
+    (options, args) = parser.parse_args()
+
+    wizard = Wizard(includes=options.include, excludes=options.exclude)
+    wizard.run()
