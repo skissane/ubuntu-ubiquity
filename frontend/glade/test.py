272dafb4f6a47b4d40672dfee2710bc8174a14fd
carranza <>
2005-07-20 17:54:38 +0000

    [project @ 65] Internationalization rules :-)

--- frontend/glade/test.py
+++ frontend/glade/test.py
@@ -3,13 +3,22 @@
 import pygtk # for testing GTK version number
 pygtk.require ('2.0')
 import gtk
-import glade
+import gtk.glade
+import gettext
+
+DIR = './locale'
+
+gettext.bindtextdomain("test.py", DIR )
+gtk.glade.bindtextdomain("test.py", DIR )
+gtk.glade.textdomain("test.py")
+gettext.textdomain("test.py")
+gettext.install("test.py",DIR,unicode=1)
 
 def main():
  # load the interface
  main_window = gtk.glade.XML('liveinstaller.glade')
  
- main_window.get_widget('bienvenida').show()
+ main_window.get_widget('welcome').show()
  main_window.get_widget('final').show()
 
  # connect the signals in the interface
@@ -23,7 +32,7 @@ class GladeHandlers:
   def on_frontend_installer_cancel(self):
     gtk.main_quit()
  
-  def on_live_installer_delete_event(self, widget, event, data=None):
+  def on_live_installer_delete_event(self, widget):
     gtk.main_quit()
 
 if __name__ == '__main__':
