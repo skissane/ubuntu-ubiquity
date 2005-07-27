441e7786f16ec93aa82952c47714515ed83c399a
carranza <>
2005-07-27 10:57:39 +0000

    [project @ 86] Fullscreen featured mode and misc

--- frontend/glade/frontend.py
+++ frontend/glade/frontend.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 #
+
 import gtk.glade
 import gnome.ui
-import os, re, gobject, gettext
+from pango import FontDescription
+from gobject import TYPE_STRING
+from os import path, walk
+from gettext import bindtextdomain, textdomain, install
+from locale import setlocale, LC_ALL
+from re import search
 
 # Define locale path
 DIR = './locale'
@@ -10,37 +16,53 @@ DIR = './locale'
 # Define timezones path
 DIRNAME = '/usr/share/zoneinfo/'
 
-# Define screens to show (per distro)
-# ['welcome', 'timezone', 'user&pass', 'hostname', 'network', 'partitioning',
-# 'progress', 'final']
-
-GUADALINEX = [1, 0, 1, 0, 1, 1, 1, 1]
-UBUNTU = [1, 1, 1, 1, 1, 1, 1, 1]
-### Sacar procentajes de progreso a partir de la cantidad de 1's ###
-
-# Distro selected
-DISTRO = GUADALINEX
-
 class FrontendInstaller:
 
   def __init__(self):
+    # set custom language
+    self.set_locales()
+    
     # load the interface
     self.main_window = gtk.glade.XML('liveinstaller.glade')
     self.show_start()
     self.show_end()
     self.fill_combo()
     
+    # set style
+    self.installer_style()
+    
     # Declare SignalHandler
     self.main_window.signal_autoconnect(GladeHandlers.__dict__)
 
-  def set_locales():
+  def set_locales(self):
     # internationalization config
-    gettext.bindtextdomain("frontend.py", DIR )
+    bindtextdomain("frontend.py", DIR )
     gtk.glade.bindtextdomain("frontend.py", DIR )
     gtk.glade.textdomain("frontend.py")
-    gettext.textdomain("frontend.py")
-    gettext.install("frontend.py", DIR, unicode=1)
+    textdomain("frontend.py")
+    install("frontend.py", DIR, unicode=1)
 
+  def installer_style(self):
+    # set screen styles
+    self.main_window.get_widget('welcome').modify_font(FontDescription('Helvetica 14'))
+    self.main_window.get_widget('welcome').modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#087021"))
+    self.main_window.get_widget('installing_title').modify_font(FontDescription('Helvetica 30'))
+    self.main_window.get_widget('installing_title').modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#087021"))
+    self.main_window.get_widget('installing_text').modify_font(FontDescription('Helvetica 16'))
+    self.main_window.get_widget('installing_text').modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#087021"))
+    self.main_window.get_widget('final').modify_font(FontDescription('Helvetica 14'))
+    self.main_window.get_widget('final').modify_fg(gtk.STATE_NORMAL, gtk.gdk.color_parse("#087021"))
+    
+    # implementation tests
+    #style = gtk.Style()
+    #style.fg[gtk.STATE_NORMAL] = self.main_window.get_widget('installing_title').get_colormap().alloc(65000,0,0)
+    #style.font = "-adobe-helvetica-bold-r-normal--*-210-*-*-*-*-*-*"
+    #self.main_window.get_widget('help_step1').set_size_request(100, 100)
+    
+    # set fullscreen mode
+    self.main_window.get_widget('live_installer').fullscreen()
+    self.main_window.get_widget('live_installer').show()
+    
 # show and design start and end page
   def show_start(self):
     welcome = self.main_window.get_widget('welcome')
@@ -56,12 +78,12 @@ class FrontendInstaller:
 
   def fill_combo(self):
     # Fill timezone GtkComboBoxEntry
-    lista = gtk.ListStore(gobject.TYPE_STRING)
+    lista = gtk.ListStore(TYPE_STRING)
     
-    for root, dirs, files in os.walk(DIRNAME, topdown=False):
+    for root, dirs, files in walk(DIRNAME, topdown=False):
       for name in files:
-	pointer = os.path.join(root, name).split(DIRNAME)[1]
-	if not re.search('right|posix|SystemV|Etc', pointer) and re.search('/', pointer):
+	pointer = path.join(root, name).split(DIRNAME)[1]
+	if not search('right|posix|SystemV|Etc', pointer) and search('/', pointer):
 	  lista.append([pointer])
     
     self.main_window.get_widget('timezone').set_model(lista)
@@ -70,48 +92,25 @@ class FrontendInstaller:
   def main(self):
     gtk.main()
 
-# get and set attributes methods
-  def set_preseed(self):
-    '''Set values to preseed in the installation proccess'''
+  def set_installing_text(self, title, text):
+    # change text screens during the installation process
+    self.main_window.get_widget('installing_title').set_text(title)
+    self.main_window.get_widget('installing_text').set_text(text)
     
-    #self.set_language("tested")
-    #self.set_timezone("tested")
-    #self.set_username("tested")
-    #self.set_password("tested")
-    #self.set_hostname("tested")
-
-  def get_language(self):
-    return self.main_window.get_widget('language').get_active_text()
-
-  def set_language(self, int):
-    self.main_window.get_widget('language').set_active(int)
-
-  def get_timezone(self):
-    return self.main_window.get_widget('timezone').get_active_text()
-
-  def set_timezone(self,int):
-    self.main_window.get_widget('timezone').set_active(int)
+#  def get_language(self):
+#    return self.main_window.get_widget('language').get_active_text()
+#
+#  def get_timezone(self):
+#    return self.main_window.get_widget('timezone').get_active_text()
 
   def get_username(self):
     return self.main_window.get_widget('username').get_property('text')
 
-  def set_username(self, text):
-    self.main_window.get_widget('username').set_property('text', text)
-
   def get_password(self):
     return self.main_window.get_widget('password').get_property('text')
 
-  def set_password(self, text):
-    self.main_window.get_widget('password').set_property('text', text)
-
   def get_hostname(self):
     return self.main_window.get_widget('hostname').get_property('text')
-
-  def set_hostname(self, text):
-    self.main_window.get_widget('hostname').set_property('text', text)
-
-  def get_progress(self):
-    return self.main_window.get_widget('progressbar').get_value()
 
   def set_progress(self, value):
     self.main_window.get_widget('progressbar').set_percentage(value)
