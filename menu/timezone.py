5a7b6174a11ba3aea1f49a541449f85c4433692a
Colin Watson <colin.watson@canonical.com>
2005-07-27 15:31:39 +0000

    pass through return value of super.run()

--- menu/timezone.py
+++ menu/timezone.py
@@ -128,6 +128,6 @@ class Timezone(WizardStep):
             # ignored for now
             return True
 
-        super(Timezone, self).run(priority, question)
+        return super(Timezone, self).run(priority, question)
 
 stepname = 'Timezone'
