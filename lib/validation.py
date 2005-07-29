7f2cda06a55e883bee4aa127bf06b158b88dfbb1
antonio <>
2005-07-29 14:07:14 +0000

    [project @ 97] ../README.txt: updated. part/peez2/: customized parted library added, README finished. lib/validation.py: last validation done (for the name of the host).

--- lib/validation.py
+++ lib/validation.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
+""" U{pylint<http://logilab.org/projects/pylint>} mark: 9.23 """
+
 # File "validation.py".
 # Validation library.
 # Created by Antonio Olmo <aolmo@emergya.info> on 26 july 2005.
-# Last modified on 27 july 2005.
+# Last modified on 29 july 2005.
 
 from string import whitespace
 
 # Index:
 # def check_username (name):
 # def check_password (passwd1, passwd2):
+# def check_hostname (name):
 # def invalid_names ():
     
 # Function "check_username" __________________________________________________
@@ -18,7 +21,7 @@ def check_username (name):
 
     """ Check the correctness of a proposed user name.
 
-        Return codes:
+        @return:
             - C{0} valid.
             - C{1} wrong length.
             - C{2} contains white spaces.
@@ -41,7 +44,7 @@ def check_password (passwd1, passwd2):
 
     """ Check the correctness of a proposed password, writen twice.
 
-        Return codes:
+        @return:
             - C{0} valid.
             - C{1} wrong length.
             - C{2} strings do not match. """
@@ -55,13 +58,33 @@ def check_password (passwd1, passwd2):
 
     return result
 
+# Function "check_hostname" __________________________________________________
+
+def check_hostname (name):
+
+    """ Check the correctness of a proposed host name.
+
+        @return:
+            - C{0} valid.
+            - C{1} wrong length.
+            - C{2} contains white spaces. """
+
+    result = 0
+
+    if len (set (name).intersection (set (whitespace))) > 0:
+        result = 2
+    elif len (name) < 3 or len (name) > 18:
+        result = 1
+
+    return result
+
 # Function "invalid_names" ___________________________________________________
 
 def invalid_names ():
 
     """ Find out which user names should not be used.
 
-        Returns a C{set} of prohibited user names. """
+        @return: a C{set} of prohibited user names. """
 
     # Minimal set:
     result = set (['root'])
--- part/peez2/README.txt
+++ part/peez2/README.txt
@@ -1,3 +1,58 @@
 
-TODO: write instructions about "peez2".
+# File "part/peez2/README.txt".
+# "peez2" brief how-to.
+# Created by Antonio Olmo <aolmo@emergya.info> on 29 july 2005.
+
+	Description
+	-----------
+
+    "peez2" is the partition assistant made for the "Junta de Andalucía" by
+"Activa Sistemas".
+
+	Disclaimer
+	----------
+
+    This software is intended for low-level operations, and is currently being
+tested by "Guadalinex 2005" development team -- although it has been written
+by others. Use it at your own risk. For the moment, and as a piece of advice,
+test it only with unmounted hard disks containing no important data.
+
+	Files
+	-----
+
+libparted1.6-12_1.6.21-1.ext3bug_i386.deb: customized parted library.
+peez2_0.7.8_i386.deb:                      Debian package.
+peez2_0.7.8.tar.gz:                        whole development tree.
+
+	Installation
+	------------
+
+1. Install the Debian package:
+	sudo dpkg --install peez2_0.7.8_i386.deb
+   Maybe you should also install or update some other packages. Usually these
+   are:
+	libc6    (>= 2.3.2.ds1-21)
+	libntfs5 (>= 1.9.4)
+2. Set "peez2" libraries directory:
+	export LD_LIBRARY=/usr/lib/peez2
+3. In order to check ext3 partitions with its sparse_super feature enabled,
+   you will have to install the modified libpart package as well:
+	sudo dpkg --install libparted1.6-12_1.6.21-1.ext3bug_i386.deb
+   Then, set:
+	export PT_FORCE_EXT3_SPARSE=yes
+
+	Usage
+	-----
+
+    See [2].
+
+	References
+	----------
+
+[1] Main source of information from "Activa Sistemas":
+    http://activasistemas.com/forja/index.php/Análisis_de_particiones_-_peez2
+[2] Usage: http://activasistemas.com/forja/index.php/Uso
+[3] Downloads: http://activasistemas.com/forja/files/peez2
+
+# End of file.
 
