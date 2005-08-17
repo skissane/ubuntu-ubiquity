4e3440187d5ce7e97f0249cd5e2e79848836cb1f
juanje <>
2005-08-17 04:55:18 +0000

    [project @ 234] Finished the copy backend

--- lib/backend/copy.py
+++ lib/backend/copy.py
@@ -3,24 +3,69 @@
 import os
 from ue import misc
 
+
 class Copy:
 
-  def __init__(self, source, target, mountpoints):
-    self.source = source
-    self.target = target
-    
+  def __init__(self, mountpoints):
+    self.source = '/source'
+    self.target = '/target'
+    self.mountpoints = mountpoints
 
-  def mount_target(self, mountpoints):
+  def run(self):
+    print '0 Preparing the target in the disc'
+    if self.mount_target():
+      print '5 Prepared the target in the disc'
+      misc.pre_log('info', 'Mounting target')
+    else:
+      misc.pre_log('error', 'Mounting target')
+      return False
+      
+    print '7 Getting the distro to copy'
+    if self.mount_source():
+      print '10 Got the distro to copy'
+      misc.pre_log('info', 'Mounting source')
+    else:
+      misc.pre_log('error', 'Mounting source')
+      return False
+      
+    print '12 Copying the distro files to the disc'
+    if self.copy_all():
+      print '85 Copied the distro files to the disc'
+      misc.pre_log('info', 'Copying distro')
+    else:
+      misc.pre_log('error', 'Copying distro')
+      return False
+      
+    print '86 Copying the logs files to the disc'
+    if self.copy_logs():
+      print '90 Copied the logs files to the disc'
+      misc.post_log('info', 'Copying logs files')
+    else:
+      misc.pre_log('error', 'Copying logs files')
+      return False
+      
+    print '91 Releasing the copied distro image'
+    if self.mount_source():
+      print '92 Released the copied distro image'
+      misc.post_log('info', 'Umounting source')
+    else:
+      misc.post_log('error', 'Umounting source')
+      return False
+     
+
+  def mount_target(self):
     if not os.path.isdir(self.target):
       os.mkdir(self.target)
-    misc.ex('mount', mountpoints['/'], self.target)
+    misc.ex('mount', self.mountpoints['/'], self.target)
 
-    for path, device in mountpoints.items():
+    for path, device in self.mountpoints.items():
       if path in ('/', 'swap'):
           continue
       path = os.path.join(self.target, path[1:])
       os.mkdir(path)
-      misc.ex('mount', device, path)
+      if not misc.ex('mount', device, path):
+        return False
+    return True
 
   def copy_all(self):
     files = []
@@ -55,6 +100,13 @@ class Copy:
     copy.stdin.close()
     copy.wait()
     
+  def copy_logs(self):
+    try:
+      misc.ex('cp', '-a', '/var/log/installer', os.path.join(self.target,'/var/log/installer'))
+    except IOError, error:
+      misc.pre_log('error', error)
+      return False
+    return True
 
   def mount_source(self):
     from os import path
@@ -78,5 +130,10 @@ class Copy:
     misc.ex('umount', self.source)
     misc.ex('losetup', '-d', self.dev)
 
+
+if __name__ == '__main__':
+  mountpoints = misc.get_vars()
+  copy = Copy(mountpoints)
+  copy.run()
 
 # vim:ai:et:sts=2:tw=80:sw=2:
