0ac3d60f9f51d2c7ccb1c3259bd78b79ad80b36f
antonio <>
2005-08-29 13:54:03 +0000

    [project @ 331] Some "peez2" functions finished. "peez2_test.py" created -- for "peez2" library testing purposes.

--- lib/backend/peez2.py
+++ lib/backend/peez2.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-""" U{pylint<http://logilab.org/projects/pylint>} mark: 8.33 """
+""" U{pylint<http://logilab.org/projects/pylint>} mark: 9.07 """
 
 # File "peez2.py".
 # Automatic partitioning with "peez2".
 # Created by Antonio Olmo <aolmo@emergya.info> on 25 august 2005.
-# Last modified on 26 august 2005.
+# Last modified on 29 august 2005.
+
+# TODO: improve "locale" detection.
 
 from popen2 import Popen3
 
@@ -15,7 +17,9 @@ from popen2 import Popen3
 # def get_commands (drive):
 # def call_peez2 (args):
 
+locale = 'es'
 binary = 'peez2'
+common_arguments = '2> /dev/null'
     
 # Function "get_drives" ______________________________________________________
 
@@ -25,14 +29,43 @@ def get_drives ():
 
     result = []
 
-    # TODO: parse I/O.
+    lines = call_peez2 () ['out']
 
-    # For every drive found:
-    drive = {'no':     0,
-             'name':   None,
-             'device': None,
-             'size':   0}
-    result.append (drive)
+    for i in lines:
+
+        if 'LD#' == i [:3]:
+            fields = i [3:].split ('|')
+
+            if 'es' == locale:
+                drive = {}
+
+                for j in fields:
+
+                    if 'Medio:' == j [:6]:
+                        drive ['no'] = int (j [6:])
+                    elif 'Modelo:' == j [:7]:
+                        drive ['name'] = j [7:]
+                    elif 'Ruta:' == j [:5]:
+                        drive ['device'] = j [5:]
+                    elif 'Total:' == j [:6]:
+                        drive ['size'] = int (j [6:])
+
+                result.append (drive)
+            elif 'en' == locale:
+                drive = {}
+
+                for j in fields:
+
+                    if 'Media:' == j [:6]:
+                        drive ['no'] = int (j [6:])
+                    elif 'Model:' == j [:6]:
+                        drive ['name'] = j [6:]
+                    elif 'Path:' == j [:5]:
+                        drive ['device'] = j [5:]
+                    elif 'Total:' == j [:6]:
+                        drive ['size'] = int (j [6:])
+
+                result.append (drive)
 
     return result
 
@@ -44,17 +77,65 @@ def get_info (drive):
 
     result = None
 
-    child = call_peez2 ('-d ' + drive)
-    # TODO: parse I/O.
-    print child
+    lines = call_peez2 ('-d ' + drive) ['out']
 
-    # If everything goes right:
-    stats = {'primary':  0,
-             'extended': 0,
-             'logical':  0}
-    parts = [None, None, None]
-    result = {'stats': stats,
-              'parts': parts}
+    for i in lines:
+
+        if 'AA#' == i [:3]:
+
+            if None == result:
+                result = {}
+
+            if not result.has_key ('warn'):
+                result ['warn'] = []
+
+            result ['warn'].append (i [3:-1])
+        elif 'VV#' == i [:3]:
+
+            if None == result:
+                result = {}
+
+            if 'es' == locale:
+
+                if 'Particiones primarias totales:' == i [3:33]:
+                    result ['prim'] = i [33:-1]
+                elif 'Particiones extendidas:' == i [3:26]:
+                    result ['ext'] = int (i [26:])
+                elif 'Particiones l' == i [3:16] and 'gicas:' == i [17:23]:
+                    result ['logic'] = int (i [23:])
+                elif 'Espacios libres:' == i [3:19]:
+                    result ['free'] = int (i [19:])
+                elif 'Particiones de linux:' == i [3:24]:
+                    result ['linux'] = int (i [24:])
+                elif 'Particiones de Windows(TM):' == i [3:30]:
+                    result ['win'] = int (i [30:])
+                elif 'Disk Status#' == i [3:15]:
+
+                    if not result.has_key ('status'):
+                        result ['status'] = []
+
+                    (result ['status']).append (i [15:-1])
+
+            elif 'en' == locale:
+
+                if 'Total primary partitions:' == i [3:28]:
+                    result ['prim'] = i [28:-1]
+                elif 'Total extended partitions:' == i [3:29]:
+                    result ['ext'] = int (i [29:])
+                elif 'Total logical partitions:' == i [3:28]:
+                    result ['logic'] = int (i [28:])
+                elif 'Total free spaces:' == i [3:21]:
+                    result ['free'] = int (i [21:])
+                elif 'Total linux partitions:' == i [3:26]:
+                    result ['linux'] = int (i [26:])
+                elif 'Total win partitions:' == i [3:24]:
+                    result ['win'] = int (i [24:])
+                elif 'Disk Status#' == i [3:15]:
+
+                    if not result.has_key ('status'):
+                        result ['status'] = []
+
+                    (result ['status']).append (i [15:-1])
 
     return result
 
@@ -77,13 +158,15 @@ def get_commands (drive):
 
 # Function "call_peez2" ______________________________________________________
 
-def call_peez2 (args):
+def call_peez2 (args = ''):
 
-    """ Executes "peez2". """
+    """ Execute "peez2" with arguments provided, if any. """
 
-    result = Popen3 (binary + ' ' + args)
+    child = Popen3 (binary + ' ' + common_arguments + ' ' + args)
 
-    return result
+    return {'out': child.fromchild,
+            'in':  child.tochild,
+            'err': child.childerr}
 
 # End of file.
 
