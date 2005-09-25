82825014fe4cc745f9e2cdac3c7b02b3974a7acd
juanje <>
2005-09-25 16:36:46 +0000

    [project @ 570] Converted into a real frontend

--- lib/frontend/netcloner.py
+++ lib/frontend/netcloner.py
@@ -21,20 +21,9 @@ class Wizard:
   def __init__(self, distro):
     # declare attributes
     self.distro = distro
-    self.checked_partitions = False
     self.pid = False
-    self.hostname = ''
-    self.fullname = ''
-    self.name = ''
-    self.password = ''
-    self.gparted = False
-    self.entries = {
-                    'hostname' : 0,
-                    'fullname' : 0, 
-                    'username' : 0,
-                    'password' : 0,
-                    'verified_password' : 0
-                    }
+    self.info = {}
+    self.parse('/etc/config.cfg',self.info)
    
     # Start a timer to see how long the user runs this program
     self.start = time.time()
@@ -44,7 +33,52 @@ class Wizard:
     
     
   def run(self):
-    pass
+    from ue import validation
+    error_msg = ['\n']
+    error = 0
+    result = validation.check_username(self.info['username'])
+    if ( result == 1 ):
+      error_msg.append("· username contains dots (they're not allowed).\n")
+      error = 1
+    elif ( result == 2 ):
+      error_msg.append("· username contains uppercase characters (they're not allowed).\n")
+      error = 1
+    elif ( result == 3 ):
+      error_msg.append("· username wrong length (allowed between 3 and 24 chars).\n")
+      error = 1
+    elif ( result == 4 ):
+      error_msg.append("· username contains white spaces (they're not allowed).\n")
+      error = 1
+    elif ( result in [5, 6] ):
+      error_msg.append("· username is already taken or prohibited.\n")
+      error = 1
+    if ( error == 1 ):
+      self.show_error(''.join(error_msg))
+    result = validation.check_password(self.info['password'], self.info['password'])
+    if ( result in [1,2] ):
+      error_msg.append("· password wrong length (allowed between 4 and 16 chars).\n")
+      error = 1
+    elif ( result == 3 ):
+      error_msg.append("· passwords don't match.\n")
+      error = 1
+    if ( error == 1 ):
+      self.show_error(''.join(error_msg))
+    result = validation.check_hostname(self.info['hostname'])
+    if ( result == 1 ):
+      error_msg.append("· hostname wrong length (allowed between 3 and 18 chars).\n")
+      error = 1
+    elif ( result == 2 ):
+      error_msg.append("· hostname contains white spaces (they're not allowed).\n")
+      error = 1
+    if ( error == 1 ):
+      self.show_error(''.join(error_msg))
+    result = validation.check_mountpoint(self.mountpoint1.get_active_text())
+    if ( result[0] is not '/' ):
+       error_msg.append("· <b>mountpoint</b> must start with '/').\n")
+       error = 1
+    if ( error == 1 ):
+      self.show_error(''.join(error_msg))
+    self.progress_loop()
 
 
   def set_locales(self):
@@ -92,23 +126,13 @@ class Wizard:
  
  
   def set_vars_file(self):
-    from ue import fmisc
-    vars = {}
-    attribs = ['hostname','fullname','username','password']
-    try:
-      for name in attribs:
-        var = getattr(self, name)
-        vars[name] = var.get_text()
-      vars['mountpoints'] = self.mountpoints
-    except:
-      pre_log('error', 'Missed attrib to write to /tmp/vars')
-      self.quit()
-    else:
-      fmisc.set_var(vars)
+    from ue import misc
+    misc.set_var(self.info)
 
 
   def show_error(self, msg):
-    pass
+    post_log('error', msg)
+    print "ERROR: " + msg
 
   def quit(self):
     if self.pid:
@@ -125,113 +149,9 @@ class Wizard:
     self.set_progress(msg)
     return True
 
-
-  def on_next_clicked(self, widget):
-    step = self.steps.get_current_page()
-    pre_log('info', 'Step_before = %d' % step)
-    # From Welcome to Info
-    if step == 0:
-      self.next.set_label('gtk-go-forward')
-      self.next.set_sensitive(False)
-      self.help.show()
-      self.steps.next_page()
-    # From Info to Part1
-    elif step == 1:
-      from ue import validation
-      error_msg = ['\n']
-      error = 0
-      for result in validation.check_username(self.username.get_property('text')):
-        if ( result == 1 ):
-          error_msg.append("· <b>username</b> contains dots (they're not allowed).\n")
-          error = 1
-        elif ( result == 2 ):
-          error_msg.append("· <b>username</b> contains uppercase characters (they're not allowed).\n")
-          error = 1
-        elif ( result == 3 ):
-          error_msg.append("· <b>username</b> wrong length (allowed between 3 and 24 chars).\n")
-          error = 1
-        elif ( result == 4 ):
-          error_msg.append("· <b>username</b> contains white spaces (they're not allowed).\n")
-          error = 1
-        elif ( result in [5, 6] ):
-          error_msg.append("· <b>username</b> is already taken or prohibited.\n")
-          error = 1
-      for result in validation.check_password(self.password.get_property('text'), self.verified_password.get_property('text')):
-        if ( result in [1,2] ):
-          error_msg.append("· <b>password</b> wrong length (allowed between 4 and 16 chars).\n")
-          error = 1
-        elif ( result == 3 ):
-          error_msg.append("· <b>passwords</b> don't match.\n")
-          error = 1
-      for result in validation.check_hostname(self.hostname.get_property('text')):
-        if ( result == 1 ):
-          error_msg.append("· <b>hostname</b> wrong length (allowed between 3 and 18 chars).\n")
-          error = 1
-        elif ( result == 2 ):
-          error_msg.append("· <b>hostname</b> contains white spaces (they're not allowed).\n")
-          error = 1
-      if ( error == 1 ):
-        self.show_error(''.join(error_msg))
-      else:
-        self.gparted_loop()
-        self.browser_vbox.destroy()
-        self.help.hide()
-        #self.next.set_sensitive(False)
-        self.steps.set_current_page(3)
-        #self.back.show()
-        #if not self.checked_partitions:
-        #  if not self.check_partitions():
-        #    return          
-        #self.steps.next_page()
-    # From Part1 to part2
-    elif step == 2 and self.gparted:
-      self.next.set_sensitive(False)
-      self.gparted_loop()
-      self.steps.next_page()
-    # From Part1 to Progress
-    elif step == 2 and not self.gparted:
-      self.progress_loop()
-      self.steps.set_current_page(5)
-    # From Part2 to Progress
-    elif step == 3:
-      for widget in [self.partition1, self.partition2, self.partition3,
-      self.partition4, self.partition5, self.partition6, self.partition7,
-      self.partition8, self.partition9, self.partition10 ]:
-        self.show_partitions(widget)
-      self.steps.next_page()
-    elif step == 4:
-      error_msg = ['\n']
-      error = 0
-      for result in validation.check_mountpoint(self.mountpoint1.get_active_text()):
-        if ( result[0] is not '/' ):
-           error_msg.append("· <b>mountpoint</b> must start with '/').\n")
-           error = 1
-      if ( error == 1 ):
-        self.show_error(''.join(error_msg))
-      else:
-        self.steps.next_page()
-    elif step == 4:
-      self.embedded.destroy()
-      self.next.set_sensitive(False)
-      self.progress_loop()
-      self.steps.next_page()
-    # From Progress to Finish
-    elif step == 5:
-      self.next.set_label('Finish and Reboot')
-      self.next.connect('clicked', lambda *x: gtk.main_quit())
-      self.back.set_label('Just Finish')
-      self.back.connect('clicked', lambda *x: gtk.main_quit())
-      self.next.set_sensitive(True)
-      self.back.show()
-      self.cancel.hide()
-      self.steps.next_page()
-    
-    step = self.steps.get_current_page()
-    pre_log('info', 'Step_after = %d' % step)
-
-
 if __name__ == '__main__':
-  w = Wizard('ubuntu')
+  distro = open('/etc/lsb-release').readline().strip().split('=')[1].lower()
+  w = Wizard(distro)
   w.run()
 
 
