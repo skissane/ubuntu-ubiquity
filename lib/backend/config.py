747a74be1f3d22065a705b4a710a9f5f39d9ae41
carranza <>
2005-11-01 14:21:58 +0000

    [project @ 1041] config fix

--- lib/backend/config.py
+++ lib/backend/config.py
@@ -209,7 +209,7 @@ class Config:
     subprocess.Popen(['chroot', self.target, 'chpasswd', '--md5'], stdin=passwd.stdout)
     self.chrex('mkdir', '/home/%s' % self.username)
     try:
-      self.chrex('/usr/local/sbin/adduser.local')
+      self.chrex('/usr/local/sbin/adduser.local', self.username)
     except Exception, e:
       for group in GROUPS:
         self.chrex('adduser', self.username, group)
