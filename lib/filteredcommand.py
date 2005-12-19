1e98549acfc17edde00a6baf11c36c83da699d86
Colin Watson <colin.watson@canonical.com>
2005-12-19 17:27:58 +0000

    * Add rudimentary FilteredCommand class, a convenience wrapper around   DebconfFilter that makes it easier to test various things.

---
+++ lib/filteredcommand.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+try:
+    from debconf import DebconfCommunicator
+except ImportError:
+    from ue.debconfcommunicator import DebconfCommunicator
+from ue.debconffilter import DebconfFilter
+
+class FilteredCommand(object):
+    def __init__(self):
+        self.package = 'espresso'
+
+    def debug(self, fmt, *args):
+        if 'ESPRESSO_DEBUG' in os.environ:
+            message = fmt % args
+            print >>sys.stderr, '%s: %s' % (self.package, message)
+
+    def run(self, command, widgets={}):
+        db = DebconfCommunicator(self.package)
+        dbfilter = DebconfFilter(db, widgets)
+
+        # TODO: Set as unseen all questions that we're going to ask.
+
+        ret = dbfilter.run(command)
+
+        if ret != 0:
+            # TODO: error message if (ret / 256) != 10
+            self.debug("%s exited with code %d", command, ret)
+
+        db.shutdown()
+
+        return ret
+
+if __name__ == '__main__':
+    import sys
+    fc = FilteredCommand()
+    fc.run(sys.argv[1])
