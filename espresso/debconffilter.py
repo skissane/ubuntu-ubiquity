6db12da8560abda8b76e76416b1f953489f4658b
Colin Watson <colin.watson@canonical.com>
2006-01-07 16:11:40 +0000

    add PROGRESS filtering to debconffilter

--- espresso/debconffilter.py
+++ espresso/debconffilter.py
@@ -56,6 +56,7 @@ class DebconfFilter:
             self.debug_re = re.compile(os.environ['DEBCONF_DEBUG'])
         else:
             self.debug_re = None
+        self.progress_bar = None
 
     def debug(self, key, *args):
         if self.debug_re is not None and self.debug_re.search(key):
@@ -142,6 +143,51 @@ class DebconfFilter:
                 for widget in self.find_widgets(question, 'metaget'):
                     self.debug('filter', 'widget found for', question)
                     widget.metaget(question, field)
+
+            if command == 'PROGRESS' and len(params) >= 1:
+                subcommand = params[0].upper()
+                if subcommand == 'START' and len(params) == 4:
+                    progress_min = int(params[1])
+                    progress_max = int(params[2])
+                    progress_title = params[3]
+                    for widget in self.find_widgets(progress_title,
+                                                    'progress_start'):
+                        self.debug('filter', 'widget found for', progress_title)
+                        widget.progress_start(progress_min, progress_max,
+                                              progress_title)
+                    self.progress_bar = progress_title
+                elif self.progress_bar is not None:
+                    if subcommand == 'SET' and len(params) == 2:
+                        progress_val = int(params[1])
+                        for widget in self.find_widgets(self.progress_bar,
+                                                        'progress_set'):
+                            self.debug('filter', 'widget found for',
+                                       self.progress_bar)
+                            widget.progress_set(self.progress_bar,
+                                                progress_val)
+                    elif subcommand == 'STEP' and len(params) == 2:
+                        progress_inc = int(params[1])
+                        for widget in self.find_widgets(self.progress_bar,
+                                                        'progress_step'):
+                            self.debug('filter', 'widget found for',
+                                       self.progress_bar)
+                            widget.progress_step(self.progress_bar,
+                                                 progress_inc)
+                    elif subcommand == 'INFO' and len(params) == 2:
+                        progress_info = params[1]
+                        for widget in self.find_widgets(self.progress_bar,
+                                                        'progress_info'):
+                            self.debug('filter', 'widget found for',
+                                       self.progress_bar)
+                            widget.progress_info(self.progress_bar,
+                                                 progress_info)
+                    elif subcommand == 'STOP' and len(params) == 1:
+                        for widget in self.find_widgets(self.progress_bar,
+                                                        'progress_stop'):
+                            self.debug('filter', 'widget found for',
+                                       self.progress_bar)
+                            widget.progress_stop(self.progress_bar)
+                        self.progress_bar = None
 
             if command == 'GO' and next_go_backup:
                 self.reply(30, 'backup', log=True)
