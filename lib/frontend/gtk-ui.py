3fba6f24177f35504bc9f09dfce3355b13a2e0f2
Colin Watson <colin.watson@canonical.com>
2006-07-07 15:44:29 +0100

    fix gtk import in frontend

--- lib/frontend/gtk-ui.py
+++ lib/frontend/gtk-ui.py
@@ -17,7 +17,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import pygtk
+pygtk.require('2.0')
+import gobject
 import gtk
+import gtk.glade
 from debconf import DebconfCommunicator
 import filteredcommand
 from components import language, keyboard, timezone, user
