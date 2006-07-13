bab8f37629ba911c2f43aac246f204ce4f770c4a
Colin Watson <colin.watson@canonical.com>
2006-07-13 12:39:23 +0100

    copyright notice and comment tweak

--- ubiquity/filteredcommand.py
+++ ubiquity/filteredcommand.py
@@ -1,6 +1,23 @@
 #! /usr/bin/python
 # -*- coding: UTF-8 -*-
 
+# Copyright (C) 2005, 2006 Canonical Ltd.
+# Written by Colin Watson <cjwatson@ubuntu.com>.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import sys
 import os
 import signal
@@ -25,8 +42,7 @@ DEBCONF_IO_HUP = 8
 class FilteredCommand(object):
     def __init__(self, frontend, db=None):
         self.frontend = frontend
-        # db does not normally need to be specified; it is only useful to
-        # set it if frontend=None.
+        # db does not normally need to be specified.
         self.db = db
         self.done = False
         self.current_question = None
