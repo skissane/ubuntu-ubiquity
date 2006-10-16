fb56f7844b32ad1dc78ed9e39ab8c43dfdce4b82
Colin Watson <colin.watson@canonical.com>
2006-10-16 23:21:46 +0100

    fix KDE frontend desync; check whether special-purpose partitions are already used for something else

--- ubiquity/frontend/kde-ui.py
+++ ubiquity/frontend/kde-ui.py
@@ -1160,6 +1160,8 @@ class Wizard:
                         (path, format, validate_filesystems[device], None)
             # Check for some special-purpose partitions detected by partman.
             for device, mountpoint in self.auto_mountpoints.iteritems():
+                if device in validate_mountpoints:
+                    continue
                 if not mountpoint.startswith('/'):
                     validate_mountpoints[device] = \
                         (mountpoint, False, None, None)
