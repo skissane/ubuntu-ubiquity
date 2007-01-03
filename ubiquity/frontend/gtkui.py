3ae56a6a7505d274bd10c1f6ff12276063eec9b5
Colin Watson <colin.watson@canonical.com>
2007-01-03 11:28:27 +0000

    revert spurious gtkui.py changes

--- ubiquity/frontend/gtkui.py
+++ ubiquity/frontend/gtkui.py
@@ -304,10 +304,7 @@ class Wizard:
                 # that's probably unavoidable for now. (We only use this for
                 # gparted, which has its own UI loop.)
                 self.allow_change_step(True)
-            print "EXEC in run()"
-
             gtk.main()
-            print "EXEC in run() done"
 
             if self.installing:
                 self.progress_loop()
@@ -408,7 +405,6 @@ class Wizard:
         text = get_string(widget.get_name(), lang)
         if text is None:
             return
-        print "text: " + str(text) + " name " + str(widget.get_name())
 
         if isinstance(widget, gtk.Label):
             name = widget.get_name()
@@ -685,7 +681,6 @@ class Wizard:
         if self.dbfilter is not None:
             self.dbfilter.cancel_handler()
         if gtk.main_level() > 0:
-            print "QUIT in quit()"
             gtk.main_quit()
 
 
@@ -802,7 +797,6 @@ class Wizard:
             # expect recursive main loops to be exited and
             # debconffilter_done() to be called when the filter exits
         elif gtk.main_level() > 0:
-            print "QUIT in on_next_clicked"
             gtk.main_quit()
 
     def on_keyboardlayoutview_row_activated(self, treeview, path, view_column):
@@ -1279,7 +1273,6 @@ class Wizard:
             # expect recursive main loops to be exited and
             # debconffilter_done() to be called when the filter exits
         elif gtk.main_level() > 0:
-            print "QUIT in on_back_clicked"
             gtk.main_quit()
 
 
@@ -1440,7 +1433,6 @@ class Wizard:
                 # and won't call run_main_loop() for itself.
                 self.allow_change_step(True)
             elif gtk.main_level() > 0:
-                print "QUIT in debconffilter_done"
                 gtk.main_quit()
 
 
@@ -2199,15 +2191,12 @@ class Wizard:
     # Run the UI's main loop until it returns control to us.
     def run_main_loop (self):
         self.allow_change_step(True)
-        print "EXEC in run_main_loop"
         gtk.main()
-        print "EXEC in run_main_loop done"
 
 
     # Return control to the next level up.
     def quit_main_loop (self):
         if gtk.main_level() > 0:
-            print "QUIT in quit_main_loop"
             gtk.main_quit()
 
 
