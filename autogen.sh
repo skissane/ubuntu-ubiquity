beadba76af30b801fe2d098e9fe78a26153734ff
Colin Watson <cjwatson@canonical.com>
2008-02-25 15:19:28 +0000

    * Use 'autoreconf -fi' so that Autotools files reliably get upgraded. * Upgrade to gettext 0.17.

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,4 @@ if ! type intltool-update >/dev/null 2>&1; then
 	echo "Install intltool and try again." >&2
 	exit 1
 fi
-AUTOMAKE=automake-1.10 ACLOCAL=aclocal-1.10 autoreconf -I m4
+AUTOMAKE=automake-1.10 ACLOCAL=aclocal-1.10 autoreconf -I m4 -fi
