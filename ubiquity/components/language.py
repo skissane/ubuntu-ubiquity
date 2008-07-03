7d3a6327a9f8c74526048a6cf12e2fc2acb85128
Evan Dandrea <evan.dandrea@canonical.com>
2008-07-03 01:04:31 -0400

    Initial localechooser work.

--- ubiquity/components/language.py
+++ ubiquity/components/language.py
@@ -20,6 +20,7 @@
 import os
 import re
 import locale
+import syslog
 
 from ubiquity.filteredcommand import FilteredCommand
 from ubiquity import i18n
@@ -28,15 +29,90 @@ from ubiquity import misc
 class Language(FilteredCommand):
     def prepare(self):
         self.language_question = None
-        self.db.fset('languagechooser/language-name', 'seen', 'false')
+        self.codes = None
+        self.names = None
+        #self.db.fset('languagechooser/language-name', 'seen', 'false')
+        #self.db.fset('localechooser/languagelist', 'seen', 'false')
         self.db.set('localechooser/alreadyrun', 'false')
         questions = ['^languagechooser/language-name',
-                     '^countrychooser/(shortlist|country-name)$']
+                     '^countrychooser/(shortlist|country-name)$',
+                     'localechooser/languagelist', 'localechooser/continentlist']
         return (['/usr/lib/ubiquity/localechooser/localechooser'], questions,
                 {'PATH': '/usr/lib/ubiquity/localechooser:' + os.environ['PATH'],
                  'OVERRIDE_SHOW_ALL_LANGUAGES': '1'})
 
+    #def subst(self, question, key, value):
+    #    syslog.syslog('subst %s %s' % (question, key))
+    #    if key == 'CODES':
+    #        self.codes = value
+    #    elif key == 'NAMES_BOTH':
+    #        self.names = value
+
     def run(self, priority, question):
+        syslog.syslog('question: %s' % question)
+        if question == 'localechooser/languagelist':
+            self.language_question = question
+            current_language_index = 17 #self.value_index(question)
+            current_language = "English"
+
+            # language_display_map = {u'Galego': (u'Galician', 'gl'), ...}
+            import gzip
+            languagelist = gzip.open('/usr/lib/ubiquity/localechooser/languagelist.data.gz')
+            language_display_map = {}
+            i = 0
+            for line in languagelist:
+                # TODO: necessary?
+                if line == '' or line == '\n':
+                    continue
+                code, name, trans = line.strip(u'\n').split(u':')[1:]
+                if code in ('dz', 'km'):
+                    i += 1
+                    continue
+                language_display_map[trans] = (name, code)
+                if i == current_language_index:
+                    current_language = trans
+                i += 1
+            languagelist.close()
+
+            def compare_choice(x, y):
+                result = cmp(language_display_map[x][1],
+                             language_display_map[y][1])
+                if result != 0:
+                    return result
+                return cmp(x, y)
+
+            sorted_choices = sorted(language_display_map, compare_choice)
+            syslog.syslog('sorted_choices: %s' % sorted_choices)
+            self.frontend.set_language_choices(sorted_choices,
+                                               language_display_map)
+            syslog.syslog('language_display_map: %s' % language_display_map)
+            self.frontend.set_language(current_language)
+
+        #if question == 'localechooser/languagelist':
+        #    syslog.syslog('codes: %s' % self.codes)
+        #    syslog.syslog('names: %s' % self.names)
+        #    language_display_map = {}
+        #    for code, name in zip(self.codes.split(', '), self.names.split(u', ')):
+        #        # FIXME evand 2008-06-16: This will break horribly once perl
+        #        # debconf supports directives.
+        #        n = name.split(u'${!TAB}-${!TAB}')
+        #        language_display_map[n[1]] = (n[0], code)
+        #    syslog.syslog('language_display_map: %s' % language_display_map)
+        #    
+        #    def compare_choice(x, y):
+        #        result = cmp(language_display_map[x][1],
+        #                     language_display_map[y][1])
+        #        if result != 0:
+        #            return result
+        #        return cmp(x, y)
+
+        #    sorted_choices = sorted(language_display_map, compare_choice)
+        #    syslog.syslog('sorted_choices: %s' % sorted_choices)
+        #    self.frontend.set_language_choices(sorted_choices,
+        #                                       language_display_map)
+        #    current_language = "English"
+        #    self.frontend.set_language(current_language)
+
         if question.startswith('languagechooser/language-name'):
             self.language_question = question
 
@@ -92,6 +168,8 @@ class Language(FilteredCommand):
             self.frontend.set_language_choices(sorted_choices,
                                                language_display_map)
             self.frontend.set_language(current_language)
+        elif question == 'localechooser/continentlist':
+            pass
 
         elif question.startswith('countrychooser/'):
             if 'DEBCONF_USE_CDEBCONF' not in os.environ:
@@ -116,8 +194,16 @@ class Language(FilteredCommand):
         return FilteredCommand.run(self, priority, question)
 
     def ok_handler(self):
+        #self.preseed('debconf/language', 'UTF-8')
+        #self.preseed('debian-installer/language', 'pt_BR_US:UTF-8')
+        #self.preseed('debian-installer/locale', 'BR')
         if self.language_question is not None:
-            self.preseed(self.language_question, self.frontend.get_language())
+            self.preseed(self.language_question, 'fr')
+            #self.preseed('debconf/language', 'UTF-8')
+            #self.preseed('debian-installer/language', 'fr_FR')
+            #self.preseed('debian-installer/country', 'FR')
+            #self.preseed('debian-installer/locale', 'fr')
+            #self.preseed(self.language_question, self.frontend.get_language())
         FilteredCommand.ok_handler(self)
 
     def cleanup(self):
