6badfa07f13d11806d1efc9164ecaa912ab2c4ca
Mario Limonciello <dell@dell-desktop>
2008-08-21 20:39:55 -0500

    hide the installer UI while we launch mythtv-setup

--- ubiquity/frontend/mythbuntu_ui.py
+++ ubiquity/frontend/mythbuntu_ui.py
@@ -460,9 +460,11 @@ class Wizard(ubiquity.frontend.gtk_ui.Wizard):
 
     def do_mythtv_setup(self,widget):
         """Spawn MythTV-Setup binary."""
-        os.seteuid(0)
-        execute("/usr/share/ubiquity/mythbuntu-setup")
-        drop_privileges()
+        self.live_installer.hide()
+        while gtk.events_pending():
+                gtk.main_iteration()
+        execute_root("/usr/share/ubiquity/mythbuntu-setup")
+        self.live_installer.show()
 
     def do_connection_test(self,widget):
         """Tests to make sure that the backend is accessible"""
