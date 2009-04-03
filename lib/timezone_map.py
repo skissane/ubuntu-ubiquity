f0bc54604ac882c241a529b4aa914ea9bf704977
Colin Watson <cjwatson@canonical.com>
2009-04-03 13:56:51 +0100

    * GTK frontend:   - Use the full size of the underlying images for the timezone map if the     screen is big enough.

--- lib/timezone_map.py
+++ lib/timezone_map.py
@@ -155,8 +155,16 @@ class TimezoneMap(gtk.Widget):
     def do_size_request(self, requisition):
         # Set a small size request to create an aspect ratio for the parent
         # widget.
-        width = self.orig_background.get_width() / 2
-        height = self.orig_background.get_height() / 2
+        screen_height = gtk.gdk.get_default_root_window().get_screen().get_height()
+        # fudge factor for rest of timezone page + panels + a bit for luck;
+        # since the current background image is 409 pixels high, 1024+768
+        # screens and better should end up with a full-sized background
+        if screen_height > self.orig_background.get_height() + 300:
+            width = self.orig_background.get_width()
+            height = self.orig_background.get_height()
+        else:
+            width = self.orig_background.get_width() / 2
+            height = self.orig_background.get_height() / 2
         requisition.width = width
         requisition.height = height
         gtk.Widget.do_size_request(self, requisition)
