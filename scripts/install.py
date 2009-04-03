e08701af4dadf1d73786b883cca9ced02d26ef4e
Colin Watson <cjwatson@canonical.com>
2009-04-03 01:34:54 +0100

    Sync with netcfg 1.44: Define IPv6 ::1 address as "localhost" (thanks, Robb Topolski; LP: #301430).

--- scripts/install.py
+++ scripts/install.py
@@ -1559,7 +1559,7 @@ exit 0"""
         print >>hosts, textwrap.dedent("""\
 
             # The following lines are desirable for IPv6 capable hosts
-            ::1     ip6-localhost ip6-loopback
+            ::1     localhost ip6-localhost ip6-loopback
             fe00::0 ip6-localnet
             ff00::0 ip6-mcastprefix
             ff02::1 ip6-allnodes
