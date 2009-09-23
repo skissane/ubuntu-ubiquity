0929135d2d36312e849a4d81854e44dd3950373e
Roman Shtylman <shtylman@gmail.com>
2009-09-22 21:07:16 -0400

    added shift key view to keyboard display

--- ubiquity/frontend/kde_components/Keyboard.py
+++ ubiquity/frontend/kde_components/Keyboard.py
@@ -16,23 +16,45 @@ class Keyboard(QWidget):
     def __init__(self, parent = None):
         QWidget.__init__(self, parent)
         
-        self.codes = []
+        self.regularCodes = []
         self.shiftCodes = []
         self.layout = None
         self.variant = None
+        self.showShift = False
         
         #load the ui
         uic.loadUi(os.path.join(uidir, "keyboard.ui"), self)
         
-        self.keys=[]
-        
+        self.keys = []
         for row in self.children():
             for key in row.children():
                 if type(key) == QPushButton:
                     index = "0%s" % key.objectName()
                     if "0x00" in index:
                         continue
-                    keys.append((key, int(index, 16)))
+                    self.keys.append((key, int(index, 16)))
+                    
+        self.x36.pressed.connect(self.shiftOn)
+        self.x2a.pressed.connect(self.shiftOn)
+        self.x36.released.connect(self.shiftOff)
+        self.x2a.released.connect(self.shiftOff)
+        
+    def shiftOn(self):
+        self.showShift = True
+        self.redrawKeys()
+        
+    def shiftOff(self):
+        self.showShift = False
+        self.redrawKeys()
+            
+    def redrawKeys(self):
+        source = self.regular_text
+        if self.showShift:
+            source = self.shift_text
+            
+        for k in self.keys:
+            k[0].setText(source(k[1]).replace("&", "&&"))
+        
         
     def setLayout(self, layout):
         self.layout = layout
@@ -41,20 +63,11 @@ class Keyboard(QWidget):
         self.variant = variant
         self.loadCodes()
         
-        for k in keys:
-            k[0].setText = self.plain_text(k[1])
-        
-        #for row in self.children():
-            #for key in row.children():
-                #if type(key) == QPushButton:
-                    #index = "0%s" % key.objectName()
-                    #if "0x00" in index:
-                        #continue
-                    #key.setText(self.plain_text(int(index, 16)))
-        
+        self.redrawKeys()
+                    
     #given a keyboard index? scancode?
     #return the unicode character
-    def plain_text(self, index):
+    def regular_text(self, index):
         
         if index == 0xe: #backspace
             return "<"
@@ -71,7 +84,33 @@ class Keyboard(QWidget):
         elif index == 0x38:
             return "Alt"
 
-        full = self.codes[index]
+        full = self.regularCodes[index]
+        code = full
+        
+        if (0xf000 & full):
+            type = (full >> 8) & 0xff
+            code = full & 0xff
+            
+        return unichr(code)
+        
+    def shift_text(self, index):
+        
+        if index == 0xe: #backspace
+            return "<"
+        elif index == 0x2a or index == 0x36:
+            return "Shift"
+        elif index == 0x1c:
+            return "Enter"
+        elif index == 0x1d:
+            return "Ctrl"
+        elif index == 0xf:
+            return "Tab"
+        elif index == 0x3a:
+            return "Caps"
+        elif index == 0x38:
+            return "Alt"
+
+        full = self.shiftCodes[index]
         code = full
         
         if (0xf000 & full):
@@ -82,7 +121,7 @@ class Keyboard(QWidget):
         
     def printCodes(self):
         counter=1
-        for c in self.codes:
+        for c in self.regularCodes:
             if counter % 9 == 0:
                 counter = 1
                 print ""
@@ -111,19 +150,31 @@ class Keyboard(QWidget):
         cfile = ret[0]
         
         #clear the current codes
-        del self.codes[:]
+        del self.regularCodes[:]
+        del self.shiftCodes[:]
 
         inNormal = False;
+        inShift = False;
         lines = cfile.split('\n')
         for l in lines:
             if inNormal and "}" in l:
                 inNormal = False;
             
-            if inNormal:
+            if inShift and "}" in l:
+                inShift = False;
+                break
+            
+            if inNormal or inShift:
                 for code in l.split(','):
                     if len(code) > 0:
-                        self.codes.append(int(code.strip(), 16))
+                        if inNormal:
+                            self.regularCodes.append(int(code.strip(), 16))
+                        else:
+                            self.shiftCodes.append(int(code.strip(), 16))
             
             if "u_short plain_map[NR_KEYS] = {" in l:
                 inNormal = True;
+                
+            if "static u_short shift_map[NR_KEYS] = {" in l:
+                inShift = True;
                 
