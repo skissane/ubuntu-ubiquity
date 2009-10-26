29749e075060cdc8940412bbb8f8d9f96a679238
Michael Terry <michael.terry@canonical.com>
2009-10-26 14:47:54 +0100

    always refresh timezone combos when entering its page

--- ubiquity/components/ubi-timezone.py
+++ ubiquity/components/ubi-timezone.py
@@ -61,8 +61,6 @@ class PageGtk(PluginUI):
 
     def fill_timezone_boxes(self):
         m = self.region_combo.get_model()
-        if m.get_iter_first():
-            return
         tz = self.controller.dbfilter
 
         # Regions are a translated shortlist of regions, followed by full list
