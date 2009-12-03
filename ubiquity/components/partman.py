ba19de0da230373f0dcf3d1c1f2ab0cd14258099
Colin Watson <cjwatson@canonical.com>
2009-12-03 16:33:16 +0000

    Fix inconsistent return types in partman.Page.snoop.

--- ubiquity/components/partman.py
+++ ubiquity/components/partman.py
@@ -138,9 +138,9 @@ class Page(Plugin):
         because this preserves ordering and is reasonably fast to convert to
         a dictionary.)"""
 
+        options = []
         try:
             snoop = open('/var/lib/partman/snoop')
-            options = []
             for line in snoop:
                 line = unicode(line.rstrip('\n'), 'utf-8', 'replace')
                 fields = line.split('\t', 1)
@@ -149,9 +149,9 @@ class Page(Plugin):
                     options.append((key, option))
                     continue
             snoop.close()
-            return options
         except IOError:
-            return {}
+            pass
+        return options
 
     def snoop_menu(self, options):
         """Parse the raw snoop data into script, argument, and displayed
