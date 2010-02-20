fd5d60d8c7a24b5841769e12bf9e741b2cda47d3
Roman Shtylman <shtylman@gmail.com>
2010-02-20 17:30:40 -0500

    anged the kde keyboard display to draw with qt
    this removed 3 svg keyboard files making the deb size a bit smaller
    also improved the clarity of the keyboard display

--- ubiquity/frontend/kde_components/Keyboard.py
+++ ubiquity/frontend/kde_components/Keyboard.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from PyQt4.QtCore import Qt, QRectF
-from PyQt4.QtGui import QWidget, QFont, QPainter
+from PyQt4.QtGui import QWidget, QFont, QPainter, QPen, QPainterPath, QColor
 from PyQt4.QtSvg import QSvgRenderer
 from PyQt4 import uic
 
@@ -21,26 +21,35 @@ def fromUnicodeString(raw):
 
 class Keyboard(QWidget):
     
-    kb_104 = [
-        (0, [0x29, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd]),
-        (55, [0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x2b]),
-        (70, [0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28]),
-        (84, [0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35]),
-        (0, [])]
+    kb_104 = {
+        "extended_return": False,
+        "keys": [
+        (0x29, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd),
+        (0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x2b),
+        (0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28),
+        (0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35),
+        ()]
+    }
         
-    kb_105 = [
-        (0, [0x29, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd]),
-        (55, [0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b]),
-        (70, [0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x2b]),
-        (45, [0x54, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35]),
-        (0, [])]
+    kb_105 = {
+        "extended_return": True,
+        "keys": [
+        (0x29, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd),
+        (0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b),
+        (0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x2b),
+        (0x54, 0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35),
+        ()]
+    }
         
-    kb_106 = [
-        (0, [0x29, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe]),
-        (55, [0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b]),
-        (70, [0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29]),
-        (94, [0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36]),
-        (0, [])]
+    kb_106 = {
+        "extended_return": True,
+        "keys": [
+        (0x29, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe),
+        (0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b),
+        (0x1e, 0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29),
+        (0x2c, 0x2d, 0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36),
+        ()]
+    }
         
     lowerFont = QFont("Helvetica", 10, QFont.DemiBold)
     upperFont = QFont("Helvetica", 8)
@@ -52,15 +61,7 @@ class Keyboard(QWidget):
         self.layout = "us"
         self.variant = ""
         
-        self.setFixedSize(565, 185)#670#.843
-        self.kw = 40 * .843
-        self.ks = 5  * .843
-        
-        self.svg = None
         self.kb = None
-        
-        #self.loadCodes()
-        #self.loadInfo()
         
     def setLayout(self, layout):
         self.layout = layout
@@ -75,45 +76,131 @@ class Keyboard(QWidget):
         kbl_104 = ["us", "th"]
         kbl_106 = ["jp"]
         
+        # most keyboards are 105 key so default to that
         if self.layout in kbl_104:
-            if self.kb != self.kb_104:
-                self.svg = QSvgRenderer(os.path.join(IMG_DIR, "104_key.svg"))
-                self.kb = self.kb_104
+            self.kb = self.kb_104
         elif self.layout in kbl_106:
-            if self.kb != self.kb_106:
-                self.svg = QSvgRenderer(os.path.join(IMG_DIR, "106_key.svg"))
-                self.kb = self.kb_106
+            self.kb = self.kb_106
         elif self.kb != self.kb_105:
-            self.svg = QSvgRenderer(os.path.join(IMG_DIR, "105_key.svg"))
             self.kb = self.kb_105
         
     def paintEvent(self, pe):
-        if not self.svg:
-            return
-    
         p = QPainter(self)
-        self.svg.render(p)
+        p.setRenderHint(QPainter.Antialiasing)
         
-        kw = self.kw
-        ks = self.ks
+        pen = QPen()
+        pen.setWidth(1)
+        pen.setColor(QColor(0x8c, 0xa3, 0xb0))
+        p.setPen(pen)
         
-        y = 2
-        for r in self.kb:
-            x = r[0] + 6
-            
-            for k in r[1]:
+        p.setBrush(QColor(0xe4, 0xec, 0xf4))
+        
+        rx = 6
+        
+        space = 6
+        w = self.width()-2;
+        kw = (w - 14 * space)/15
+        
+        def drawRow(row, sx, sy, last_end=False):            
+            x=sx
+            y=sy
+            keys = row
+            rw=w-sx
+            i=0
+            for k in keys:
+                rect = QRectF(x, y, kw, kw)
+                
+                if i == len(keys)-1 and last_end:
+                    rect.setWidth(rw)
+                
+                p.drawRoundedRect(rect, rx, rx)
+                p.setPen(Qt.black)
+                
+                rect.adjust(5,1, 0, 0)
+                
                 p.setFont(self.lowerFont)
-                rect = QRectF(x, y, kw, kw-1)
                 p.drawText(rect, Qt.AlignLeft | Qt.AlignBottom, self.regular_text(k))
                 
                 p.setFont(self.upperFont)
                 p.drawText(rect, Qt.AlignLeft | Qt.AlignTop, self.shift_text(k))
                 
-                #p.drawText(x, y, kw-10, kw, Qt.AlignRight | Qt.AlignBottom, self.ctrl_text(k))
-                #p.drawText(x, y, kw-10, kw, Qt.AlignRight | Qt.AlignTop, self.alt_text(k))
-                x += kw + ks
-            y += ks + kw
-    
+                rw = rw - space - kw
+                x = x + space + kw
+                i = i+1
+                
+                p.setPen(pen)
+            return (x,rw)
+        
+        x=.5
+        y=.5
+        
+        keys = self.kb["keys"]
+        ext_return = self.kb["extended_return"]
+        
+        first_key_w = 0
+        
+        rows = 4
+        remaining_x = [0,0,0,0]
+        remaining_widths = [0,0,0,0]
+        
+        for i in range(0, rows):
+            if first_key_w > 0:
+                first_key_w = first_key_w*1.375
+                
+                if self.kb == self.kb_105 and i==3:
+                    first_key_w = kw * 1.275
+                
+                rect = QRectF(x, y, first_key_w, kw)
+                p.drawRoundedRect(rect, rx, rx)
+                x = x + first_key_w + space
+            else:
+                first_key_w = kw
+            
+            x,rw = drawRow(keys[i], x, y, i==1 and not ext_return)
+            
+            remaining_x[i] = x
+            remaining_widths[i] = rw
+            
+            if i!=1 and i!=2:
+                rect = QRectF(x, y, rw, kw)
+                p.drawRoundedRect(rect, rx, rx)
+            
+            x=.5
+            y = y + space + kw
+            
+        if ext_return:
+            rx=rx*2
+            x1 = remaining_x[1]
+            y1 = .5 + kw*1 + space*1
+            w1 = remaining_widths[1]
+            x2 = remaining_x[2]
+            y2 = .5 + kw*2 + space*2
+            w2 = remaining_widths[2]
+            
+            # this is some serious crap... but it has to be so
+            # maybe one day keyboards won't look like this...
+            # one can only hope
+            pp = QPainterPath()
+            pp.moveTo(x1, y1+rx)
+            pp.arcTo(x1, y1, rx, rx, 180, -90)
+            pp.lineTo(x1+w1-rx, y1)
+            pp.arcTo(x1+w1-rx, y1, rx, rx, 90, -90)
+            pp.lineTo(x1+w1, y2+kw-rx)
+            pp.arcTo(x1+w1-rx, y2+kw-rx, rx, rx, 0, -90)
+            pp.lineTo(x2+rx, y2+kw)
+            pp.arcTo(x2, y2+kw-rx, rx, rx, -90, -90)
+            pp.lineTo(x2, y1+kw)
+            pp.lineTo(x1+rx, y1+kw)
+            pp.arcTo(x1, y1+kw-rx, rx, rx, -90, -90)
+            pp.closeSubpath()
+            
+            p.drawPath(pp)
+        else:
+            x= remaining_x[2]
+            y = .5 + kw*2 + space*2
+            rect = QRectF(x, y, remaining_widths[2], kw)
+            p.drawRoundedRect(rect, rx, rx)
+        
         QWidget.paintEvent(self, pe)
         
     def regular_text(self, index):
@@ -166,7 +253,7 @@ class Keyboard(QWidget):
 
 ## testing 
 if __name__ == "__main__":
-    from PyQt4.QtGui import *
+    from PyQt4.QtGui import QApplication, QVBoxLayout
     
     IMG_DIR = "../../../gui/qt/images"
     
@@ -185,6 +272,7 @@ if __name__ == "__main__":
     addKb("gb")
     addKb("th")
     addKb("gr")
+    addKb("jp")
 
     win.show()
 
