ac81605b9366b271d25331740630a6de5965b849
Colin Watson <cjwatson@canonical.com>
2010-02-25 11:35:37 +0000

    * KDE frontend:   - Fix partman component for use_as signature change (LP: #527468).

--- ubiquity/frontend/kde_components/PartMan.py
+++ ubiquity/frontend/kde_components/PartMan.py
@@ -166,7 +166,7 @@ class PartMan(QWidget):
         self.create_dialog.partition_create_place_beginning.setChecked(True)
 
         self.create_use_method_names = {}
-        for method, name, description in self.ctrlr.dbfilter.create_use_as(devpart):
+        for method, name, description in self.ctrlr.dbfilter.use_as(devpart, True):
             self.create_use_method_names[description] = name
             self.create_dialog.partition_create_use_combo.addItem(description)
         if self.create_dialog.partition_create_use_combo.count() == 0:
