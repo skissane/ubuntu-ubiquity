70a03009f9b518efc4fba8b9982121fab1ccd842
Evan Dandrea <evan.dandrea@canonical.com>
2010-03-31 11:01:40 +0100

    Accept and discard arguments to the KDE frontend's on_keyboard_layout_selected function (LP: #552399).

--- ubiquity/components/ubi-console-setup.py
+++ ubiquity/components/ubi-console-setup.py
@@ -271,7 +271,7 @@ class PageKde(PluginUI):
         self.plugin_widgets = self.page
 
     @only_this_page
-    def on_keyboard_layout_selected(self):
+    def on_keyboard_layout_selected(self, *args):
         layout = self.get_keyboard()
         l = self.controller.dbfilter.get_locale()
         if layout is not None:
