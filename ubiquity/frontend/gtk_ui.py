73bd3ba643944408dcf7417f4e863cc3db2c6369
Evan Dandrea <evan.dandrea@canonical.com>
2010-04-07 17:17:30 +0100

    Fix backing up past partitioning when manual partitioning was selected (LP: #557210).

--- ubiquity/frontend/gtk_ui.py
+++ ubiquity/frontend/gtk_ui.py
@@ -816,6 +816,13 @@ class Wizard(BaseFrontend):
     def add_history(self, page, widget):
         history_entry = (page, widget)
         if self.history:
+            # We may have skipped past child pages of the component.  Remove
+            # the history between the page we're on and the end of the list in
+            # that case.
+            if history_entry in self.history:
+                idx = self.history.index(history_entry)
+                if idx + 1 < len(self.history):
+                    self.history = self.history[:idx+1]
             # We may have either jumped backward or forward over pages.
             # Correct history in that case
             new_index = self.pages.index(page)
