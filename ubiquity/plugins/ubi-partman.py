0066a146ef7c8b5d829c08edebea51aa3f07a901
Colin Watson <cjwatson@canonical.com>
2010-06-25 11:22:39 +0100

    Add btrfs support.

--- ubiquity/plugins/ubi-partman.py
+++ ubiquity/plugins/ubi-partman.py
@@ -793,7 +793,8 @@ class PageGtk(PageBase):
         # If the selected method isn't a filesystem, then selecting a mount
         # point makes no sense. TODO cjwatson 2007-01-31: Unfortunately we
         # have to hardcode the list of known filesystems here.
-        known_filesystems = ('ext4', 'ext3', 'ext2', 'reiserfs', 'jfs', 'xfs',
+        known_filesystems = ('ext4', 'ext3', 'ext2',
+                             'btrfs', 'reiserfs', 'jfs', 'xfs',
                              'fat16', 'fat32', 'ntfs', 'uboot')
         if iterator is None or model[iterator][0] not in known_filesystems:
             self.partition_edit_mount_combo.child.set_text('')
