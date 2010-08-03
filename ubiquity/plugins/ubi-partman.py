1c15515b4f1ceed8041b7e7cb81b5c4b91060106
Evan Dandrea <evan.dandrea@canonical.com>
2010-08-03 17:26:29 -0400

    Fix partitioning page not showing due to an exception being raised in its constructor.

--- ubiquity/plugins/ubi-partman.py
+++ ubiquity/plugins/ubi-partman.py
@@ -156,7 +156,7 @@ class PageGtk(PageBase):
             self.current_page = self.page
 
             # Set some parameters that do not change between runs of the plugin
-            release = misc.get_release()
+            release = get_release()
             self.partitionbox.set_property('title', release.name)
             # New partition
             self.resizewidget.get_child2().child.set_property('title', release.name)
