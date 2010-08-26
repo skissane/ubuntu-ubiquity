b1399b39432fddb51655ec0574b867b8a9a1f6f8
Mario Limonciello <Mario_Limonciello@Dell.com>
2010-08-25 22:02:14 -0500

    Set the buttons on the second autopartitioning screen appropriately to the choices on the first.

--- ubiquity/plugins/ubi-partman.py
+++ ubiquity/plugins/ubi-partman.py
@@ -248,8 +248,11 @@ class PageGtk(PageBase):
         the ask page.'''
         if self.resize_use_free.get_active():
             self.partition_container.set_current_page(0)
+            s = self.controller.get_string('part_auto_use_entire_disk')
         elif self.use_device.get_active():
             self.partition_container.set_current_page(1)
+            s = self.controller.get_string('part_auto_split_largest_partition')
+        self.part_auto_use_entire_disk.set_label(s)
 
     def part_auto_select_drive_changed (self, unused_widget):
         '''The user has selected a different disk drive from the drop down.
