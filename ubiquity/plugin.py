091511e334bb26327477276ef34a6989a65efdf2
Evan Dandrea <evan.dandrea@canonical.com>
2010-10-10 16:29:56 +0100

    Nevermind that last commit.  It feels wrong.

--- ubiquity/plugin.py
+++ ubiquity/plugin.py
@@ -32,7 +32,7 @@ class Plugin(FilteredCommand):
         return None
 
 class InstallPlugin(Plugin):
-    def install(self, target, progress, *args, **kwargs):
+    def install(self, *args, **kwargs):
         return self.run_command(auto_process=True)
 
 # Use this as a decorator if you want to guard against a function being called
