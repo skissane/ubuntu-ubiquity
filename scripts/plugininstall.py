c97b0b1b8e83b3400e5d7effd501329902d08426
Colin Watson <cjwatson@canonical.com>
2010-11-15 12:05:50 +0000

    Remove 'localhost' from default ::1 line in /etc/hosts (see netcfg 1.57, Debian #595107).

--- scripts/plugininstall.py
+++ scripts/plugininstall.py
@@ -258,7 +258,7 @@ class Install(install_misc.InstallBase):
         print >>hosts, textwrap.dedent("""\
 
             # The following lines are desirable for IPv6 capable hosts
-            ::1     localhost ip6-localhost ip6-loopback
+            ::1     ip6-localhost ip6-loopback
             fe00::0 ip6-localnet
             ff00::0 ip6-mcastprefix
             ff02::1 ip6-allnodes
