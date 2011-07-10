de50cfaa0d4cb3fc8e051361c882b703ee87cadb
Evan Dandrea <evan.dandrea@canonical.com>
2011-07-10 20:11:28 +0100

    add gtkwidgets tests.

---
+++ tests/test_gtkwidgets.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import unittest
+from ubiquity import gtkwidgets, segmented_bar, timezone_map, tz
+from test import test_support
+from gi.repository import Gtk
+
+class WidgetTests(unittest.TestCase):
+    def setUp(self):
+        self.err = None
+        def excepthook(type, value, tb):
+            Gtk.main_quit()
+            self.err = type(value)
+        import sys
+        sys.excepthook = excepthook
+        self.win = Gtk.Window()
+
+    def tearDown(self):
+        self.win.hide()
+        if self.err:
+            raise self.err
+
+    def test_segmented_bar(self):
+        sb = segmented_bar.SegmentedBar()
+        self.win.add(sb)
+        sb.add_segment_rgb('Test One', 30 * 1000 * 1000 * 1000, 'ff00ff')
+        sb.add_segment_rgb('Test Two', 30 * 1000 * 1000 * 1000, '0000ff')
+        for segment in sb.segments:
+            self.assertEqual(segment.subtitle, '30.0 GB')
+        self.assertEqual(sb.segments[0].title, 'Test One')
+        self.assertEqual(sb.segments[1].title, 'Test Two')
+        sb.remove_all()
+        self.assertEqual(sb.segments, [])
+        self.win.show_all()
+        # TODO delete me
+        #self.win.connect('destroy', Gtk.main_quit)
+        #Gtk.main()
+
+    def test_timezone_map(self):
+        tzdb = tz.Database()
+        tzmap = timezone_map.TimezoneMap(tzdb, 'pixmaps/timezone')
+        self.win.add(tzmap)
+        self.win.show_all()
+        Gtk.main()
+        
+        
+if __name__ == '__main__':
+    test_support.run_unittest(WidgetTests)
