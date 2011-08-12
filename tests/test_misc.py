618495774f98cbd158e006697ee60a599b6cfccb
Evan Dandrea <evan.dandrea@canonical.com>
2011-08-13 00:18:00 +0100

    Fix indicator keymaps test

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -134,19 +134,15 @@ class MiscTests(unittest.TestCase):
 
     @mock.patch('ubiquity.gconftool.set_list')
     def test_set_indicator_keymaps(self, mock_set_list):
-        args = ('/desktop/gnome/peripherals/keyboard/kbd/layouts', 'string',
-                ['us', 'gh', 'gb', 'ie', 'za', 'ng', 'ph', 'us\taltgr-intl',
-                 'us\thbs', 'in\teng', 'ca\teng', 'us\teuro', 'us\tintl',
-                 'us\talt-intl', 'us\tcolemak', 'us\tdvorak',
-                 'us\tdvorak-intl', 'us\tdvorak-alt-intl', 'us\tdvorak-l',
-                 'us\tdvorak-r', 'us\tdvorak-classic', 'us\tdvp', 'us\tmac',
-                 'us\tolpc2', 'gh\tgeneric', 'gh\tgillbt', 'gb\textd',
-                 'gb\tintl', 'gb\tdvorak', 'gb\tdvorakukp', 'gb\tmac',
-                 'gb\tmac_intl', 'gb\tcolemak', 'ie\tUnicodeExpert',
-                 'ie\togam', 'ie\togam_is434', 'ph\tcapewell-dvorak',
-                 'ph\tcapewell-qwerf2k6', 'ph\tcolemak', 'ph\tdvorak'])
         misc.set_indicator_keymaps('en_US.UTF-8')
-        mock_set_list.assert_called_once_with(*args)
+        self.assertEqual(mock_set_list.call_count, 1)
+        self.assertEqual(mock_set_list.call_args[0][0],
+            '/desktop/gnome/peripherals/keyboard/kbd/layouts')
+        self.assertEqual(mock_set_list.call_args[0][1], 'string')
+        self.assertIn('us', mock_set_list.call_args[0][2])
+        self.assertIn('gb', mock_set_list.call_args[0][2])
+        self.assertIn('gb\tintl', mock_set_list.call_args[0][2])
+        self.assertIn('gb\tmac', mock_set_list.call_args[0][2])
 
 #class PartedServerTests(unittest.TestCase):
 #    def setUp(self):
