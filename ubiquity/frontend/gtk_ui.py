5ffb79220cb1b286d79c133479befff766b8a3fd
Stéphane Graber <stgraber@ubuntu.com>
2011-08-28 18:54:30 -0400

    Replace Gdk.CONTROL_MASK by GI equivalent Gdk.ModifierType.CONTROL_MASK

--- ubiquity/frontend/gtk_ui.py
+++ ubiquity/frontend/gtk_ui.py
@@ -507,9 +507,9 @@ class Wizard(BaseFrontend):
             self.thunar_set_volmanrc(self.thunar_previous)
 
     def a11y_profile_keys(self, window, event):
-        if event.state & Gdk.CONTROL_MASK and event.keyval == Gdk.keyval_from_name('h'):
+        if event.state & Gdk.ModifierType.CONTROL_MASK and event.keyval == Gdk.keyval_from_name('h'):
             self.a11y_profile_high_contrast_activate()
-        elif event.state & Gdk.CONTROL_MASK and event.keyval == Gdk.keyval_from_name('s'):
+        elif event.state & Gdk.ModifierType.CONTROL_MASK and event.keyval == Gdk.keyval_from_name('s'):
             self.a11y_profile_screen_reader_activate()
 
     def a11y_profile_high_contrast_activate(self, widget=None):
