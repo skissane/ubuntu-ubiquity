c5973a87195abeff634551853893ebfbc6a13539
Stéphane Graber <stgraber@ubuntu.com>
2011-08-28 15:58:18 -0400

    Apparently the call to encode() in preseed() (filteredcommand.py) is actually breaking cases where value contains utf-8. Dropping the call makes Ubiquity works fine with UTF-8 input (tested with gtk version and French install).

--- ubiquity/filteredcommand.py
+++ ubiquity/filteredcommand.py
@@ -333,7 +333,6 @@ class FilteredCommand(UntrustedBase):
 
     def preseed(self, name, value, seen=True):
         value = misc.debconf_escape(value)
-        value = value.encode("UTF-8", "ignore")
         try:
             self.db.set(name, value)
         except debconf.DebconfError:
