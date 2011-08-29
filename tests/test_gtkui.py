a1105d86ca794e8cbd8d31bf1f856f21433f87b5
Colin Watson <cjwatson@canonical.com>
2011-08-29 17:21:50 +0100

    Mock ubiquity.nm.wireless_hardware_present in GTK frontend tests, as it may not be able to talk to D-Bus.

--- tests/test_gtkui.py
+++ tests/test_gtkui.py
@@ -15,7 +15,9 @@ class TestFrontend(unittest.TestCase):
     @mock.patch('ubiquity.misc.execute')
     @mock.patch('ubiquity.frontend.base.drop_privileges')
     @mock.patch('ubiquity.frontend.gtk_ui.Wizard.customize_installer')
-    def test_question_dialog(self, *args):
+    @mock.patch('ubiquity.nm.wireless_hardware_present')
+    def test_question_dialog(self, mock_wireless_hardware_present, *args):
+        mock_wireless_hardware_present.return_value = False
         ui = gtk_ui.Wizard('test-ubiquity')
         with mock.patch('gi.repository.Gtk.Dialog.run') as run:
             run.return_value = 0
