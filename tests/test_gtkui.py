42f2abee059605e07c67e66d00d7383881264197
Evan Dandrea <evan.dandrea@canonical.com>
2011-10-19 17:07:04 +0100

    Add a unit test for ensuring that the entire interface is translatable.

--- tests/test_gtkui.py
+++ tests/test_gtkui.py
@@ -54,3 +54,53 @@ class TestFrontend(unittest.TestCase):
         print alloc.height
         self.assertLessEqual(alloc.width, 640)
         self.assertLessEqual(alloc.height, 500)
+
+    def test_interface_translated(self):
+        from ubiquity.frontend import gtk_ui
+        from gi.repository import Gtk
+        ui = gtk_ui.Wizard('test-ubiquity')
+        missing_translations = []
+        with mock.patch_object(ui, 'translate_widget') as translate_widget:
+            def side_effect(widget, lang=None, prefix=None):
+                label  = isinstance(widget, Gtk.Label)
+                button = isinstance(widget, Gtk.Button)
+                # We have some checkbuttons without labels.
+                button = button and widget.get_label()
+                # Stock buttons.
+                button = button and not widget.get_use_stock()
+                window = isinstance(widget, Gtk.Window)
+                if not (label or button or window):
+                    return
+                name = widget.get_name()
+                if not ui.get_string(name, lang, prefix):
+                    missing_translations.append(name)
+            translate_widget.side_effect = side_effect
+            ui.translate_widgets()
+            whitelist = [
+                # These are calculated and set as the partitioning options are
+                # being calculated.
+                'reuse_partition_desc', 'reuse_partition_title',
+                'replace_partition_desc', 'replace_partition_title',
+                'resize_use_free_desc', 'resize_use_free_title',
+                'use_device_desc', 'use_device_title', 'part_ask_heading',
+                # Pulled straight from debconf when the installation medium is
+                # already mounted.
+                'part_advanced_warning_message',
+                # These are caculated and set inside info_loop in the user
+                # setup page.
+                'password_strength', 'hostname_error_label',
+                'password_error_label', 'username_error_label',
+                # Pulled straight from debconf into the UI on progress.
+                'install_progress_text',
+                # Contains just the traceback.
+                'crash_detail_label',
+                # Pages define a debconf template to look up and use as the
+                # title. If it is not set or not found, the title is hidden.
+                'page_title',
+                ]
+            missing_translations = set(missing_translations) - set(whitelist)
+            missing_translations = list(missing_translations)
+            if missing_translations:
+                missing_translations = ', '.join(missing_translations)
+                raise Exception('Missing translation for:\n%s'
+                                % missing_translations)
