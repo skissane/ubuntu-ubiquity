9f89e8dd2100bd41faf6424497c23e48631d4a78
Colin Watson <cjwatson@canonical.com>
2012-02-12 02:35:06 +0000

    webcam: Warn when trying to get unknown properties.

--- src/webcam/webcam.c
+++ src/webcam/webcam.c
@@ -76,6 +76,9 @@ ubiquity_webcam_get_property (GObject *object, guint prop_id, GValue *value,
 		case PROP_BUTTON:
 			g_value_set_object (value, (GObject *) priv->button);
 			break;
+		default:
+			G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id,
+							   pspec);
 	}
 }
 
