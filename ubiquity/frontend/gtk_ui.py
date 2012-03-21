469dc247ed7b556f17cfd28fae3b1a026187f390
Colin Watson <cjwatson@canonical.com>
2012-03-21 01:35:50 +0000

    Make sure to load translations for "USB" and "CD" on the language page (LP: #958249).

--- ubiquity/frontend/gtk_ui.py
+++ ubiquity/frontend/gtk_ui.py
@@ -967,6 +967,8 @@ color : @fg_color
         core_names.append('ubiquity/imported/default-ltr')
         core_names.append('ubiquity/text/release_notes_only')
         core_names.append('ubiquity/text/update_installer_only')
+        core_names.append('ubiquity/text/USB')
+        core_names.append('ubiquity/text/CD')
         for stock_item in ('cancel', 'close', 'go-back', 'go-forward',
                             'ok', 'quit'):
             core_names.append('ubiquity/imported/%s' % stock_item)
