92a8a3b74dd6866d03f2c8fe3edd64dd63e87077
Brian Murray <brian@canonical.com>
2012-03-23 10:51:15 -0700

    UTF-8-encode debug messages which are written to stderr (LP: #960278)

--- ubiquity/filteredcommand.py
+++ ubiquity/filteredcommand.py
@@ -65,7 +65,8 @@ class UntrustedBase(object):
             # bizarre time formatting code per syslogd
             time_str = time.ctime()[4:19]
             message = fmt % args
-            print >>sys.stderr, '%s %s: %s' % (time_str, PACKAGE, message)
+            print >>sys.stderr, (u'%s %s: %s' %
+                (time_str, PACKAGE, message)).encode('utf-8')
 
 class FilteredCommand(UntrustedBase):
     def __init__(self, frontend, db=None, ui=None):
