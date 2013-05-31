88f05e1448f7ef7608a8ec51cc950f00e12687bb
Dmitrijs Ledkovs <dmitrijs.ledkovs@canonical.com>
2013-05-31 15:52:20 +0100

    Finish

--- autopilot/ubiquity/tests/test_welcome.py
+++ autopilot/ubiquity/tests/test_welcome.py
@@ -1,19 +1,30 @@
+import os
+from collections import namedtuple
 from autopilot.testcase import AutopilotTestCase
 from autopilot.introspection import get_autopilot_proxy_object_for_process
+from testtools.matchers import Equals
 
 class WelcomeTests(AutopilotTestCase):
 
+    def setUp(self):
+        super(WelcomeTests, self).setUp()
+        self.app = self.launch_application()
+    
     def launch_application(self):
         '''
         Hmm... launch ubiquity
 
         :returns: The application proxy object.
         '''
-        from collections import namedtuple
-        Pr = namedtumpe('Process', ['pid'])
-        my_process = Pr(os.environ['UBIQUITY_PID'])
+
+        Pr = namedtuple('Process', ['pid'])
+        my_process = Pr(int(os.environ['UBIQUITY_PID']))
         return get_autopilot_proxy_object_for_process(my_process)
         
-    def test_something(self):
-        '''An example test case that will always pass.'''
-        self.assertTrue(True)
+    def test_window_title(self):
+        '''
+        Check that title is "Install"
+        '''
+        main_window = self.app.select_single(
+            'GtkWindow', name='live_installer')
+        self.assertThat(main_window.title, Equals("Install"))
