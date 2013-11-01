05f2dd7241d114f647779d03212628a0abfe712f
Dan Chapman daniel@chapman-mail.com <>
2013-11-01 18:52:53 +0000

    Moved alot out of the emulators and into the __init__ now we have self.expectThat

--- autopilot/ubiquity_autopilot_tests/unittests/test_expects.py
+++ autopilot/ubiquity_autopilot_tests/unittests/test_expects.py
@@ -1,12 +1,21 @@
 __author__ = 'dan'
 import unittest
-from testtools.testcase import TestCase
+from ubiquity_autopilot_tests.testcase import UbiquityTestCase
+from testtools.matchers import (
+    Equals,
+    NotEquals,
+)
+
+class ExpectTestCase(UbiquityTestCase):
 
 
-class ExpectTestCase(TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+    def test_fail_expect_equal(self):
+        self.expectEqual(5, 4)
+        self.assertThat(len(self.non_fatal_errors), Equals(1))
 
+    def test_expect_equal(self):
+        self.expectEqual(5, 5)
+        self.assertThat(len(self.non_fatal_errors), Equals(0))
 
 if __name__ == '__main__':
     unittest.main()
