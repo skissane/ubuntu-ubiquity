992944ac397c80c4813e0e1ab02df7575566d886
Steve Langasek <steve.langasek@canonical.com>
2019-10-07 15:45:02 -0700

    fix up for wrong import-dsc that pulled d-i source into the git repo confusingly

--- d-i/source/grub-installer/rescue.d/81grub-efi-force-removable.tst
+++
@@ -1,3 +0,0 @@
-#! /bin/sh -e
-[ -f /target/boot/grub/grub.cfg ] && ( grep -q /boot/efi /target/etc/fstab )
-
--- d-i/source/partconf/xasprintf.h
+++
@@ -1,6 +0,0 @@
-#ifndef XASPRINTF_H_
-#define XASPRINTF_H_ 1
-
-char    *xasprintf(const char *format, ...);
-
-#endif /* XASPRINTF_H_ */
